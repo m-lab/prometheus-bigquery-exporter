f90ebc2d127ffb9a5e930953b43a98149099f294
Stephen Soltesz <soltesz@google.com>
2017-11-03 12:10:13 -0400

    Update var and receiver names, use slice reference in Collect

--- bq/bq.go
+++ bq/bq.go
@@ -8,9 +8,9 @@ import (
 )
 
 type Collector struct {
-	runner QueryRunner
-	name   string
-	query  string
+	runner     QueryRunner
+	metricName string
+	query      string
 
 	valType prometheus.ValueType
 	desc    *prometheus.Desc
@@ -22,64 +22,68 @@ type Collector struct {
 // NewCollector creates a new BigQuery Collector instance.
 func NewCollector(runner QueryRunner, valType prometheus.ValueType, metricName, query string) *Collector {
 	return &Collector{
-		runner:  runner,
-		name:    metricName,
-		query:   query,
-		valType: valType,
-		desc:    nil,
-		metrics: nil,
-		mux:     sync.Mutex{},
+		runner:     runner,
+		metricName: metricName,
+		query:      query,
+		valType:    valType,
+		desc:       nil,
+		metrics:    nil,
+		mux:        sync.Mutex{},
 	}
 }
 
 // Describe satisfies the prometheus.Collector interface. Describe is called
 // immediately after registering the collector.
-func (bq *Collector) Describe(ch chan<- *prometheus.Desc) {
-	if bq.desc == nil {
+func (col *Collector) Describe(ch chan<- *prometheus.Desc) {
+	if col.desc == nil {
 		// TODO: collect metrics for query exec time.
-		bq.Update()
-		bq.setDesc()
+		col.Update()
+		col.setDesc()
 	}
 	// NOTE: if Update returns no metrics, this will fail.
-	ch <- bq.desc
+	ch <- col.desc
 }
 
 // Collect satisfies the prometheus.Collector interface. Collect reports values
 // from cached metrics.
-func (bq *Collector) Collect(ch chan<- prometheus.Metric) {
-	bq.mux.Lock()
-	defer bq.mux.Unlock()
+func (col *Collector) Collect(ch chan<- prometheus.Metric) {
+	col.mux.Lock()
+	// Get reference to current metrics slice to allow Update to run concurrently.
+	metrics := col.metrics
+	col.mux.Unlock()
 
-	for i := range bq.metrics {
+	for i := range col.metrics {
 		ch <- prometheus.MustNewConstMetric(
-			bq.desc, bq.valType, bq.metrics[i].value, bq.metrics[i].values...)
+			col.desc, col.valType, metrics[i].value, metrics[i].values...)
 	}
 }
 
 // String satisfies the Stringer interface. String returns the metric name.
-func (bq *Collector) String() string {
-	return bq.name
+func (col *Collector) String() string {
+	return col.metricName
 }
 
 // Update runs the collector query and atomically updates the cached metrics.
 // Update is called automaticlly after the collector is registered.
-func (bq *Collector) Update() error {
-	metrics, err := bq.runner.Query(bq.query)
+func (col *Collector) Update() error {
+	metrics, err := col.runner.Query(col.query)
 	if err != nil {
 		return err
 	}
 	// Swap the cached metrics.
-	bq.mux.Lock()
-	defer bq.mux.Unlock()
-	bq.metrics = metrics
+	col.mux.Lock()
+	defer col.mux.Unlock()
+	// Replace slice reference with new value returned from Query. References
+	// to the previous value of col.metrics are not affected.
+	col.metrics = metrics
 	return nil
 }
 
-func (bq *Collector) setDesc() {
+func (col *Collector) setDesc() {
 	// The query may return no results.
-	if len(bq.metrics) > 0 {
+	if len(col.metrics) > 0 {
 		// TODO: allow passing meaningful help text.
-		bq.desc = prometheus.NewDesc(bq.name, "help text", bq.metrics[0].labels, nil)
+		col.desc = prometheus.NewDesc(col.metricName, "help text", col.metrics[0].labels, nil)
 	} else {
 		// TODO: this is a problem.
 		return
